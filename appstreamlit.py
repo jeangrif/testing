b4bc30b967bc55772fc8e66defbfd6c8b43acb12
jeangrif <99396034+jeangrif@users.noreply.github.com>
2023-10-03 11:42:08 +0200

    Update appstreamlit.py

--- appstreamlit.py
+++ appstreamlit.py
@@ -9,17 +9,18 @@ if uploaded_file is not None:
     df = df[(df['compoA'] != '?') & (df['compoB'].notna())& (df['compoA'].notna())& (df['compoB'] != '?')]
     df = df.reset_index(drop=True)
     st.sidebar.title("Choisir la formation de l'équipe A")
-    compoA = st.sidebar.selectbox("Choix formation équipe A",df["compoA"].unique())
+    compoA = st.sidebar.multiselect("Choix formation équipe A",df["compoA"].unique())
     st.sidebar.title("Choisir la formation de l'équipe B")
-    compoB = st.sidebar.selectbox("Choix formation équipe B",df["compoB"].unique())
+    compoB = st.sidebar.multiselect("Choix formation équipe B",df["compoB"].unique())
     st.sidebar.title("Choisir les continents")
     continents = st.sidebar.multiselect("Choix continent(s)", df["Continent"].unique())
     influence = st.sidebar.slider("Affluence",0,200,max(df["influence"]))
-    if len(continents)==0:
-        new_df = df[(((df['compoA'] == compoA) & (df['compoB'] == compoB)) | ((df['compoB'] == compoA) & (df['compoA'] == compoB))) & (df["influence"] >= influence)]
+    if (len(compoA)>0) & (len(compoB)>0):
+        if len(continents)==0:
+            new_df = df[(((df['compoA'].isin(compoA)) & (df['compoB'].isin(compoB))) | ((df['compoB'].isin(compoA)) & (df['compoA'].isin(compoB)))) & (df["influence"] >= influence)]
 
-    else :
-        new_df = df[(((df['compoA'] == compoA) & (df['compoB'] == compoB)) | ((df['compoB'] == compoA) & (df['compoA'] == compoB)))& (df["influence"]>=influence)&(df["Continent"].isin(continents))]
-    new_df = new_df.reset_index(drop=True)
-    st.title("Liste des matchs")
-    st.write(new_df)
+        else :
+            new_df = df[(((df['compoA'].isin(compoA)) & (df['compoB'].isin(compoB))) | ((df['compoB'].isin(compoA)) & (df['compoA'].isin(compoB))))& (df["influence"]>=influence)&(df["Continent"].isin(continents))]
+        new_df = new_df.reset_index(drop=True)
+        st.title("Liste des matchs")
+        st.write(new_df)
